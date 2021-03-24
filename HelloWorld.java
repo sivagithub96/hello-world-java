fc0068a719cc2e191401bbb2bfc49cb7d7df5dce
EC2 Default User <ec2-user@ip-172-31-9-101.ap-south-1.compute.internal>
2021-03-24 07:02:38 +0000

    adding corona

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world, hello corona, hello covid19!");
+    System.out.println("Hello world");
+    System.out.println("Hello corona");
+    System.out.println("Hello covid19");
+     System.out.println("Hello siva");
   }
 }
