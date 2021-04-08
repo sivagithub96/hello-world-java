72ec29e834ac0c082ab4ed0207e153c1f42798b1
sivagithub96 <80754705+sivagithub96@users.noreply.github.com>
2021-04-08 16:47:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world");
-    System.out.println("Hello corona");
-    System.out.println("Hello covid19");
-     System.out.println("Hello siva");
   }
 }
